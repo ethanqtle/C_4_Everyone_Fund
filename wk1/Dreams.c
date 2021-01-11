67e64dd24bf82b8a71e3e85074e7fa80d2355886
Ethan Le <ethanqtle@gmail.com>
2021-01-10 17:05:56 -0800

    Update and save at the end if week 1

---
+++ wk1/Dreams.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main(void)
+{
+    printf("Dreams\n");
+    printf("\n");
+    printf("Hold fast to dreams\n");
+    printf("For if dreams die\n");
+    printf("Life is a broken-winged bird\n");
+    printf("That cannot fly.\n");
+    printf("\n");
+    printf("Hold fast to dreams\n");
+    printf("For when dreams go\n");
+    printf("Life is a barren field\n");
+    printf("Frozen with snow.\n");
+    printf("\n");
+    printf("Langston Hughes\n");
+    return 0;
+}
